eab15d9b70d12108b26959b223813bc70a32b087
NAGATA Tomoko <21716069@jwu.ac.jp>
2019-10-07 16:46:05 +0900

    rational3.py

--- rational.py
+++ rational.py
@@ -6,13 +6,35 @@ class Q(object):
         if self.b==1:
             return str(self.a)
         return f'{self.a}/{self.b}'
+
     def add(self, q):
         a=self.a
         b=self.b
         c=q.a
         d=q.b
-        return Q(self.c, self.d)
+        return Q(a*d+b*c, b*d)
+    def sub(self, q):
+        a=self.a
+        b=self.b
+        c=q.a
+        d=q.b
+        return Q(a*d-b*c, b*d)
+    def __mul__(self, q):
+        a=self.a
+        b=self.b
+        c=q.a
+        d=q.b
+        return Q(a*c, b*d)
+    def __truediv__(self, q):
+        a=self.a
+        b=self.b
+        c=q.a
+        d=q.b
+        return Q(a*d, b*c)
 
-    q1=Q(1, 2)
-    q2=Q(1, 3)
-    print(q1.add(q2))
+q1=Q(1, 2)
+q2=Q(1, 3)
+print(q1.add(q2))
+print(q1.sub(q2))
+print(q1*q2)
+print(q1/q2)
