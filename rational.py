61ccef29b5e3286c7ee4402b8c12b61fe3a37ead
NAGATA Tomoko <21716069@jwu.ac.jp>
2019-10-07 16:24:28 +0900

    rational2

--- rational.py
+++ rational.py
@@ -1,7 +1,18 @@
 class Q(object):
-    def __init__(self, a, b):
+    def __init__(self, a, b=1):
         self.a=a
         self.b=b
+    def __repr__(self):
+        if self.b==1:
+            return str(self.a)
+        return f'{self.a}/{self.b}'
+    def add(self, q):
+        a=self.a
+        b=self.b
+        c=q.a
+        d=q.b
+        return Q(self.c, self.d)
 
-q=Q(1, 2)
-print(q)
+    q1=Q(1, 2)
+    q2=Q(1, 3)
+    print(q1.add(q2))
