6e058e2fe0ba9694c7f81e334b7efa0903828031
NAGATA Tomoko <21716069@jwu.ac.jp>
2019-10-07 16:50:51 +0900

    rational_sub.py

---
+++ rational2.py
@@ -0,0 +1,42 @@
+import math
+class Q(object):
+    def __init__(self, a, b=1):
+        gcd=math.gcd(a, b)
+        self.a=a//gcd
+        self.b=b//gcd
+    def __repr__(self):
+        if self.b==1:
+            return str(self.a)
+        return f'{self.a}/{self.b}'
+
+    def add(self, q):
+        a=self.a
+        b=self.b
+        c=q.a
+        d=q.b
+        return Q(a*d+b*c, b*d)
+    def sub(self, q):
+        a=self.a
+        b=self.b
+        c=q.a
+        d=q.b
+        return Q(a*d-b*c, b*d)
+    def __mul__(self, q):
+        a=self.a
+        b=self.b
+        c=q.a
+        d=q.b
+        return Q(a*c, b*d)
+    def __truediv__(self, q):
+        a=self.a
+        b=self.b
+        c=q.a
+        d=q.b
+        return Q(a*d, b*c)
+
+q1=Q(1, 2)
+q2=Q(1, 3)
+print(q1.add(q2))
+#print(q1.sub(q2))
+#print(q1*q2)
+#print(q1/q2)
