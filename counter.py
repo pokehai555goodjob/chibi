d8a3d39c74cea71c0a13b958e105c1244f53604c
NAGATA Tomoko <21716069@jwu.ac.jp>
2019-10-07 15:44:20 +0900

    count1

---
+++ counter.py
@@ -0,0 +1,23 @@
+class Counter(object):
+    def __init__(self): #コンストラクタ
+        self.cnt=0
+    def count(self):
+        self.cnt += 1
+
+    def doublecount(self)
+    ＃2回カウントする
+    pass
+
+    def reset(self):
+        self.cnt=0
+
+    def show(self):
+        print(self.cnt)
+
+    def __repr__(self):
+        return str(self.cnt)
+
+c=Counter()
+c.show()
+c.doublecount()
+c.show()
