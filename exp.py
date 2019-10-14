1ad25b5c3fea83846c0c137d7f6ce87044598da8
NAGATA Tomoko <21716069@jwu.ac.jp>
2019-10-14 16:38:36 +0900

    day10/14-2

--- exp.py
+++ exp.py
@@ -1,8 +1,8 @@
 
+class Expr(object):
+    pass
 
-
-
-class Val(object):
+class Val(Expr):
     __slots__ = ['value']
     def __init__(self, value = 0):
         self.value = value
@@ -15,6 +15,9 @@ v = Val(1)
 print(v)
 #assert v.eval() == 1
 
+#assert isinstance(v, Expr) #==>True
+#assert isinstance(v, Val) #==>True
+#assert isinstance(v, int) #==>Fak
 
 class Add(object):
     __slots__=['left', 'right']
@@ -24,9 +27,22 @@ class Add(object):
     def eval(self):
         return self.left.eval() + self.right.eval()
 
-e =Add(Val(1), Val(2))
-print(e.eval())
-assert e.eval() ==3
-e= Add(Val(1), Add(Val(2), Val(3)))
-print(e.eval())
-assert e.eval() ==6
+class Div(object):
+    __slots__=['left', 'right']
+    def __init__(self,a ,b):
+        self.left =a
+        self.right=b
+    def eval(self):
+        return self.left.eval() / self.right.eval()
+
+#e =Add(Val(1), Val(2))
+#print(e.eval())
+#assert e.eval() ==3
+#e= Add(Val(1), Add(Val(2), Val(3)))
+#print(e.eval())
+#assert e.eval() ==6
+
+e=Div(Val(7),Val(2)) #7//2
+assert e.eval() == 3
+#assert isinstance(Val(1), Expr)
+#assert isinstance(Div(Val(7), Val(2)), Expr)
