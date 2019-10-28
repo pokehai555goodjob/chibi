8b904c6634c6f7ab32e33ce8bd254b37494c451a
NAGATA Tomoko <21716069@jwu.ac.jp>
2019-10-28 16:53:58 +0900

    output_tocyuu.py

--- parser.py
+++ parser.py
@@ -15,6 +15,7 @@ s2 = s[pos+1:]
 print(s, s1, s2)
 '''
 def parse(s: str):
+    '''
     if s.find('+') > 0:
         pos = s.find('+') 
         s1 = s[0:pos]
@@ -25,14 +26,23 @@ def parse(s: str):
         s1 = s[0:pos]
         s2 = s[pos+1:]
         return Mul(parse(s1), parse(s2))
+        '''
+    if s.find('-') > 0 :       
+        pos = s.rfind('-') 
+        s1 = s[0:pos]
+        s2 = s[pos+1:]
+        return Div(parse(s1), parse(s2))
     return Val(int(s))
-    '''
+'''
     else:
         s1 = s[0:pos]
         s2 = s[pos+1:]
         return Add(Val(int(s1)), Val(int(s2)))
 
-    '''
+
 e = parse("1*2+3")
 print(e, e.eval())
-
+'''
+e = parse("1-2-3")
+print(e, e.eval())
+#assert e.eval() == -4
