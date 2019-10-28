0547b69da0af14f50c545d1b079c11688645bee8
NAGATA Tomoko <21716069@jwu.ac.jp>
2019-10-28 15:57:02 +0900

    output2.py

--- parser.py
+++ parser.py
@@ -13,3 +13,14 @@ print('pos', pos)
 s1 = s[0:pos]
 s2 = s[pos+1:]
 print(s1, s2)
+
+class Add(object):
+    __slots__=['left', 'right']
+    def __init__(self,a ,b):
+        self.left =a
+        self.right=b
+    def eval(self):
+        return self.left.eval() + self.right.eval()
+
+e =Add(Val(1), Val(2))
+print(e.eval())
