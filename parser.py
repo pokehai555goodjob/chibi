705cafa6b5d2bb7997dc4a4c1454e1cb2594251d
NAGATA Tomoko <21716069@jwu.ac.jp>
2019-10-28 16:38:18 +0900

    output4.py

--- parser.py
+++ parser.py
@@ -15,14 +15,24 @@ s2 = s[pos+1:]
 print(s, s1, s2)
 '''
 def parse(s: str):
-    pos = s.find('+')
-    if pos == -1:
-        return Val(int(s))
+    if s.find('+') > 0:
+        pos = s.find('+') 
+        s1 = s[0:pos]
+        s2 = s[pos+1:]
+        return Add(parse(s1), parse(s2))
+    if s.find('*') > 0 :       
+        pos = s.find('*') 
+        s1 = s[0:pos]
+        s2 = s[pos+1:]
+        return Mul(parse(s1), parse(s2))
+    return Val(int(s))
+    '''
     else:
         s1 = s[0:pos]
         s2 = s[pos+1:]
         return Add(Val(int(s1)), Val(int(s2)))
 
+    '''
+e = parse("1*2+3")
+print(e, e.eval())
 
-e = parse("123+456")
-print(e)
