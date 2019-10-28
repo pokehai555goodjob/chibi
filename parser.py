94b8669a7f791e4561ed5ca16d0349db08c57f8c
NAGATA Tomoko <21716069@jwu.ac.jp>
2019-10-28 16:20:59 +0900

    output3.py

--- parser.py
+++ parser.py
@@ -1,8 +1,8 @@
-from exp import Val, Add
+from exp_kaitou import Val, Add, Sub, Mul, Div
 def parse(s: str):
     num = int(s)
     return Val(num)
-
+'''
 e = parse("123")
 print(e)
 
@@ -12,15 +12,17 @@ print('pos', pos)
 
 s1 = s[0:pos]
 s2 = s[pos+1:]
-print(s1, s2)
+print(s, s1, s2)
+'''
+def parse(s: str):
+    pos = s.find('+')
+    if pos == -1:
+        return Val(int(s))
+    else:
+        s1 = s[0:pos]
+        s2 = s[pos+1:]
+        return Add(parse(s1), parse(s2))
 
-class Add(object):
-    __slots__=['left', 'right']
-    def __init__(self,a ,b):
-        self.left =a
-        self.right=b
-    def eval(self):
-        return self.left.eval() + self.right.eval()
 
-e =Add(Val(1), Val(2))
-print(e.eval())
+e = parse("123+456+789")
+print(e)
